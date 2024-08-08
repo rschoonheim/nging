e851104bb71c56ef006c79efb11bd0602d10667c
romano schoonheim <stal@romanoschoonheim.nl>
2024-08-08 09:43:39 +0200

    Remove unused types.

--- src/types/math/dimension.types.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @description Dimension in 2D space.
- */
-export type Dimension2 = {
-    width_min: number;
-    width_max: number;
-    height_min: number;
-    height_max: number;
-}
--- src/types/math/vector.types.ts
+++
@@ -1,4 +0,0 @@
-/**
- * @description Vector for 2D space.
- */
-export type Vector2 = { x: number, y: number }
